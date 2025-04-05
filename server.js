90aee672f1dbaf3a57751427d6caece3f302c438
JamesTan <jamestan1496@gmail.com>
2025-04-05 19:36:12 +0400

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 
@@ -66,6 +67,9 @@ app.use('/images', express.static(path.join(__dirname,  'images')));
 
 app.get('/images/tabletennis.jpg', (req, res) => {
     logActivity("Request for image received");
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
 
 
